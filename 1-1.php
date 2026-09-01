25f8a02ecb92c47972a2e5db330653e557ee5e0c
Sudhanshu Tiwari <sudhanshu.124939@marwadiuniversity.ac.in>
2026-09-01 10:05:26 +0530

    Add files via upload

---
+++ 1-1.php
@@ -0,0 +1,15 @@
+<!-- 1.1 Write a PHP program for print Previous semester Result using variables & constants in PHP. -->
+
+<?php
+    $semester = "Fall 2023";
+    $studentName = "John Doe";
+    $mathGrade = 85;
+    $scienceGrade = 90;
+    $englishGrade = 78;
+
+    echo "Semester: " . $semester . "<br>";
+    echo "Student Name: " . $studentName . "<br>";
+    echo "Math Grade: " . $mathGrade . "<br>";
+    echo "Science Grade: " . $scienceGrade . "<br>";
+    echo "English Grade: " . $englishGrade . "<br>";
+?>
