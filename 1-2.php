e2ef86fd64d31af0fd840d9e319d1edb71ef86a7
Sudhanshu Tiwari <sudhanshu.124939@marwadiuniversity.ac.in>
2026-09-01 10:05:54 +0530

    Add files via upload

---
+++ 1-2.php
@@ -0,0 +1,14 @@
+<!-- 1.2 Write a PHP program to find out maximum and minimum number. -->
+
+<?php
+    $num1 = 10;
+    $num2 = 20;
+
+    if ($num1 > $num2) {
+        echo "Maximum number is: " . $num1 . "<br>";
+        echo "Minimum number is: " . $num2 . "<br>";
+    } else {
+        echo "Maximum number is: " . $num2 . "<br>";
+        echo "Minimum number is: " . $num1 . "<br>";
+    }
+?>
